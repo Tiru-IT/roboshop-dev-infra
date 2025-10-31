2787fdb164c9e927441bc6368077a4666d7f2a65
unknown <tirusatrapu36@gmail.com>
2025-10-31 16:26:15 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -25,9 +25,15 @@ resource "terraform_data" "mongodb" {
     host = aws_instance.mongodb.private_ip
   }
 
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
   provisioner "remote-exec" {
     inline = [ 
-        "echo hello world"
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh"
      ]
   }
 }
