5339cd9c5f0397c073f84c83205df2fc2f7a65d4
unknown <tirusatrapu36@gmail.com>
2025-10-31 21:09:53 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -36,3 +36,23 @@ resource "aws_security_group_rule" "redis_bastion" {
   source_security_group_id = local.bastion_sg_id
 
 }
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.rabbitmq_sg_id
+  source_security_group_id = local.bastion_sg_id
+
+}
+
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.mysql_sg_id
+  source_security_group_id = local.bastion_sg_id
+
+}
--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -4,6 +4,8 @@ locals {
   database_subnets_ids = split(",", data.aws_ssm_parameter.database_subnets_ids.value)[0]
 
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+  rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+  mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   
   common_name = "${var.project_name}-${var.environment}"
   common_tags ={
