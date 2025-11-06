7fabac2843bbd6bda21e3fd9a1dee17cf5f7739e
unknown <tirusatrapu36@gmail.com>
2025-11-06 09:59:55 +0530

    terraform

--- 20-bastion/locals.tf
+++ 20-bastion/locals.tf
@@ -5,6 +5,6 @@ locals {
   }
   common_name = "${var.project_name}-${var.environment}"
   ami_id = data.aws_ami.joindevops.id 
-  sg_id = data.aws_ssm_parameter.sg_id.value
+  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
   pub_ids = split(",", data.aws_ssm_parameter.public_subnets_ids.value)[0]
 }
