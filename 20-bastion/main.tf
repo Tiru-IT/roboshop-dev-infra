1e2536429d6efc5358ffa7011b98883ce31441fd
unknown <tirusatrapu36@gmail.com>
2025-11-08 08:45:31 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -5,7 +5,10 @@ resource "aws_instance" "bastion" {
   subnet_id = local.pub_ids
   iam_instance_profile = aws_iam_instance_profile.bastion.name
   user_data = file("bastion.sh")
-
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or gp2 dependas on your laptop
+  }
   tags = merge(
     local.common_tags,
     {
--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -25,3 +25,6 @@ data "aws_ssm_parameter" "private_subnets_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnets_ids"
 }
 
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project_name}/${var.environment}/vpc_id"
+}
