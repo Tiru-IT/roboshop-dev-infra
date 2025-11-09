b3720fe7871afbcea5919c36a01f68083cc5daed
unknown <tirusatrapu36@gmail.com>
2025-11-09 20:16:10 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -192,7 +192,7 @@ resource "aws_lb_listener_rule" "static" {
 
 
 
-resource "terraform_data" "catalogue" {
+resource "terraform_data" "catalogue_local" {
   triggers_replace = [
     aws_instance.catalogue.id
   ]
