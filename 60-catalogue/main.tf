c0d59e4b2929ebdc69e165b8f10fec223bb5fcf3
unknown <tirusatrapu36@gmail.com>
2025-11-10 10:43:37 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -86,6 +86,7 @@ resource "aws_launch_template" "catalogue" {
   instance_initiated_shutdown_behavior = "terminate"
 
   instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
 
   
   tag_specifications {
