0d52ba0a410ce10e96e9b007ff49fd689944e523
unknown <tirusatrapu36@gmail.com>
2025-11-15 13:30:55 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -241,6 +241,17 @@ resource "aws_security_group_rule" "frontend_frontend_alb" {
   source_security_group_id = local.frontend_alb_sg_id
 }
 
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.frontend_alb_sg_id
+  source_security_group_id = local.bastion_sg_id
+}
+
+
+
 
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
