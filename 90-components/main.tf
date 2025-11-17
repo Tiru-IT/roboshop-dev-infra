9d96dfd22bf249b9da8ce4372cefdcd218340397
unknown <tirusatrapu36@gmail.com>
2025-11-17 18:16:45 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,13 @@
+# module "components" {
+#   for_each = var.components
+#   source = "../../terraform-roboshop-component"
+#   component = each.key
+#   rool_priority = each.value.rool_priority
+# }
+
 module "components" {
-  for_each = var.components
-  source = "../../terraform-roboshop-component"
-  component = each.key
-  rool_priority = each.value.rool_priority
+    for_each = var.components
+    source = "git::https://github.com/Tiru-IT/terraform-roboshop-component.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
 }
